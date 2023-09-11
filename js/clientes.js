e2a9210857cfda15285327182b8a4b6fe57e5a6b
lcsouza695 <140920658+lcsouza695@users.noreply.github.com>
2023-09-11 12:08:24 -0300

    Add files via upload

---
+++ js/clientes.js
@@ -0,0 +1,31 @@
+var enderecoForm = document.querySelector(".form-body");
+var cepInput = document.querySelector("#cep");
+var ruaInput = document.querySelector("#rua");
+var bairroInput = document.querySelector("#bairro");
+var cidadeInput = document.querySelector("#cidade");
+var estadoInput = document.querySelector("#estado");
+var formInput = document.querySelector("[data-input]");
+
+
+
+cepInput.addEventListener("Keyup", (e) =>{
+  const inputValue = e.target.value;
+  if(inputValue.length === 8){
+      getEndereco(inputvalue);
+      //fazer alguma coisa - rodar a aplicação
+    }
+});
+
+var getEndereco = async (cep) => {
+    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    console.log(formInput);
+    console.log(data.erro); 
+    ruaInput.value = data.logradouro;
+    cidadeInput.value = data.localidade;
+    bairroInput.value = data.bairro;
+    estadoInput.value = data.uf;
+
+}
